e7e393fabd04cbda32af8462499c604973867194
george <kesaev@gmail.com>
2020-03-03 00:14:40 +0200

    improved:  - added ffmpeg install  - add 'yes' flag to install commands  - added python-dev of all latest versions

--- tello/install_av_packages.sh
+++ tello/install_av_packages.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 # General dependencies
-sudo apt-get install -y python-dev pkg-config
+sudo apt-get install -y python3-dev python3.6-dev python3.7-dev pkg-config
 
 # Library components
 sudo apt-get install -y \
     libavformat-dev libavcodec-dev libavdevice-dev \
-    libavutil-dev libswscale-dev libswresample-dev libavfilter-dev
+    libavutil-dev libswscale-dev libswresample-dev libavfilter-dev \
+    libavresample-dev
 
-# for pi only:
-sudo apt-get install libatals-base-dev
-sudo apt-get install libjasper-dev
-sudo apt-get install libqtgui4
-sudo apt-get install python3-pyqt5
-sudo apt-get install python3-pyqt4
+# for pi only: maybe not sure
+sudo apt-get install -y libatals-base-dev
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y python3-pyqt5
+sudo apt-get install -y python3-pyqt4
+
+# for all
+sudo apt install ffmpeg
